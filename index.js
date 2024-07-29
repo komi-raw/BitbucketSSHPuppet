54e3922c1cb5d5df7ab55bf5b51559946a59f5d0
Félix PEREIRA <felix.pereira@comelit.fr>
2024-07-29 15:14:31 +0200

    feat: can publish key over bitbucket

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const ENV = configDotenv();
 
 async function main(){
     if(ENV.parsed?.USERMAIL === undefined || ENV.parsed?.USERPWD === undefined){
-        console.log('ERROR : EMAIL OR PWD NOT PROVIDED')
+        console.error('ERROR : EMAIL OR PWD NOT PROVIDED')
         return;
     }
     const browser = await puppeteer.launch({headless: false});
-    const BC = new Bitbucket(browser, ENV.parsed.USERMAIL, ENV.parsed.USERPWD, ENV.parsed.USERKEY);
-    BC.process();
+    const BC = new Bitbucket(browser, ENV.parsed.USERMAIL, ENV.parsed.USERPWD, ENV.parsed.USERKEY, ENV.parsed.USERKEYLABEL);
+    const result = await BC.process();
+    if(BC.flag){
+        console.error(result);
+    } else {
+        console.log(result);
+    }
+    browser.close();
+    return;
 }
 
 main();
--- pages/Bitbucket.js
+++ pages/Bitbucket.js
@@ -3,19 +3,50 @@ export class Bitbucket {
     _tokenPage = "https://id.atlassian.com/login/security-screen";
     _bitbucket = 'https://bitbucket.org/';
 
-    constructor(browser, mail, pwd, key) {
+    flag = false;
+
+    constructor(browser, mail, pwd, key, keylabel) {
         this.browser = browser;
         this.mail = mail;
         this.pwd = pwd;
         this.key = key;
+        this.keylabel = keylabel;
     }
 
     async process() {
         this.page = await this.browser.newPage();
         await this.page.setViewport({ width: 1080, height: 1024 });
         await this.page.goto(this._loginPage);
-        await this.login();
+        const loginSuccess = await this.login();
+        if(!loginSuccess) {
+            this.flag = true;
+            return "User not found.";
+        }
         await this.settings();
+        const keyAdded = await this.ssh();
+        if(!keyAdded){
+            this.flag = true;
+            return "Key not added, maybe already existing ?";
+        }    
+        return "Ok, Success.";
+    }
+
+    async ssh(){
+        await this.page.waitForNavigation({ waitUntil: "networkidle0" });
+        await this.page.waitForSelector('#add-key');
+        await this.page.locator('#add-key').click();
+        await this.page.waitForSelector('#id_label');
+        await this.page.keyboard.press("Tab");
+        await this.page.keyboard.type(this.keylabel);
+        await this.page.keyboard.press("Tab");
+        await this.page.keyboard.type(this.key);
+
+        for(let i = 0; i < 4; i++) await this.page.keyboard.press("Tab");
+        await this.page.keyboard.press("Enter");
+        
+        //VERIFICATION
+
+        return true;
     }
 
     async settings()
@@ -39,12 +70,18 @@ export class Bitbucket {
         await this.page.waitForSelector("#password");
         await this.page.locator("#password").fill(this.pwd);
         await sendLogin();
-
+        await this.page.locator()
+        try {
+            await this.page.waitForSelector('section[data-testid="form-error"]', {timeout: 1000})
+            return false;
+        } catch(ex) {
+        }
         await this.page.waitForNavigation({ waitUntil: "networkidle0" });
         
         await this.handleIfTokenPage();
         
         await this.page.goto(this._bitbucket);
+        return true;
     }
 
     async handleIfTokenPage() {
